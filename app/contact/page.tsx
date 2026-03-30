e396977d0dbb510062e2dd82c1a322fc991df0a9
freelance-dev <dev@freelance.com>
2026-03-30 23:52:46 +0530

    Move FAQ below contact form on contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -12,8 +12,8 @@ export default function Contact() {
         </div>
       </div>
       <ContactSection />
-      <WhyTrustSection />
       <FAQSection />
+      <WhyTrustSection />
     </div>
   );
 }
